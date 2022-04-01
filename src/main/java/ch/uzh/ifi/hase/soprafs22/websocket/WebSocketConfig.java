503ff2d8f5bca0cc095e3baca37dda3e49cfdd79
Luca Zwahlen <luca.zwahlen@uzh.ch>
2022-04-01 17:48:54 +0200

    reverted back to allowedOrigins: all

--- src/main/java/ch/uzh/ifi/hase/soprafs22/websocket/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/websocket/WebSocketConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -24,8 +27,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/websocket-test").setAllowedOrigins("https://braendi-dog-client.herokuapp.com", "http://localhost:3000", "http://localhost:5000").withSockJS();
-        registry.addEndpoint("/gameupdates").setAllowedOrigins("https://braendi-dog-client.herokuapp.com").withSockJS();
+        registry.addEndpoint("/websocket-test").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/gameupdates").setAllowedOriginPatterns("*").withSockJS();
 
     }
 
