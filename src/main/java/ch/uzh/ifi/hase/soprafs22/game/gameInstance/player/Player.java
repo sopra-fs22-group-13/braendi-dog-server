8c03f42436e72ce794d60771b09e48e154e81ea6
Anton Crazzolara <anton.crazzolara@outlook.com>
2022-04-10 12:50:59 +0200

    Implemented CardStack and Player. Wrote tests for both. Player is still missing the method getFormattedCards(). Complemented Card class.

--- src/main/java/ch/uzh/ifi/hase/soprafs22/game/gameInstance/player/Player.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/game/gameInstance/player/Player.java
@@ -2,15 +2,56 @@ package ch.uzh.ifi.hase.soprafs22.game.gameInstance.player;
 
 import ch.uzh.ifi.hase.soprafs22.game.constants.COLOR;
 import ch.uzh.ifi.hase.soprafs22.game.gameInstance.cards.Card;
+import com.sun.istack.NotNull;
 
 import java.util.ArrayList;
 
 public class Player {
 
-    private ArrayList<Card> _hand = new ArrayList<Card>();
-    private COLOR _turn;
+    private final ArrayList<Card> _hand = new ArrayList<Card>();
+    private final COLOR _turn;
 
-    public Player(){
+    public Player(@NotNull int playerNr) throws IllegalArgumentException{
+        if (playerNr < 0 || playerNr > 3) throw new IllegalArgumentException("Illegal player number. PlayerNr must be in range [0,3]");
 
+        this._turn = COLOR.values()[playerNr];
+    }
+
+    public void removeCard(Card reqCard) {
+        for (Card card: _hand) {
+            if (reqCard.equalsContent(card)) {
+                _hand.remove(card);
+                return;
+            }
+        }
+    }
+
+    public boolean isCardAvailable(Card reqCard) {
+        for (Card card: _hand) {
+            if (reqCard.equalsContent(card)) return true;
+        }
+
+        return false;
+    }
+
+    public void addCard(Card card) {
+        _hand.add(card);
+    }
+
+    public int getCardCount() {
+        return _hand.size();
+    }
+
+    /**
+     * TODO
+     */
+    /*
+    public String getFormattedCard() {
+
+    }
+     */
+
+    public COLOR getTurn() {
+        return this._turn;
     }
 }
---
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/game/gameInstance/player/PlayerTest.java
@@ -0,0 +1,91 @@
+package ch.uzh.ifi.hase.soprafs22.game.gameInstance.player;
+
+import ch.uzh.ifi.hase.soprafs22.game.constants.CARDSUITE;
+import ch.uzh.ifi.hase.soprafs22.game.constants.CARDTYPE;
+import ch.uzh.ifi.hase.soprafs22.game.constants.CARDVALUE;
+import ch.uzh.ifi.hase.soprafs22.game.constants.COLOR;
+import ch.uzh.ifi.hase.soprafs22.game.gameInstance.cards.Card;
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+    Player player;
+
+    @BeforeEach
+    public void setup() {
+        player = new Player(0);
+    }
+
+    /**
+     * currently only checks if color is assigned correctly
+     */
+    @Test
+    public void creationTest() {
+        List<Player> players = List.of(player, new Player(1), new Player(2), new Player(3));
+        for (int i=0; i<4; i++) assertEquals(COLOR.values()[i], players.get(i).getTurn());
+    }
+
+    /**
+     * (1) checks preconditions
+     * (2) checks if card is successfully removed
+     * (3) checks what happens if we want to remove a card that isn't in hand
+     */
+    @Test
+    void removeCard() {
+        //adds a card and checks if it's there (just to be sure)
+        player.addCard(new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS));
+        assertEquals(1, player.getCardCount());
+        assertTrue(player.isCardAvailable(new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS)));
+
+        //removes the card again and checks if it's gone
+        player.removeCard(new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS));
+        assertEquals(0, player.getCardCount());
+        assertFalse(player.isCardAvailable(new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS)));
+
+        //removes a card that shouldn't be there anymore (shouldn't change anything)
+        player.removeCard(new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS));
+        assertEquals(0, player.getCardCount());
+        assertFalse(player.isCardAvailable(new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS)));
+    }
+
+    /**
+     * (1) tests if the card was successfully added
+     * (2) tests if card is found by copy
+     * (3) tests if second card is successfully added
+     * (4) tests if first card is still there after adding a different card
+     */
+    @Test
+    void handTest() {
+        Card firstCard = new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS);
+        Card firstCardCOPY = new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS);
+        player.addCard(firstCard);
+        assertTrue(player.isCardAvailable(firstCard));
+        assertTrue(player.isCardAvailable(firstCardCOPY));
+
+        Card secondCard = new Card(CARDVALUE.FOUR, CARDTYPE.DEFAULT, CARDSUITE.SPADES);
+        player.addCard(secondCard);
+        assertTrue(player.isCardAvailable(secondCard));
+        assertTrue(player.isCardAvailable(firstCard));
+    }
+
+    /**
+     * (1) test with empty hand
+     * (2) test with one card in hand
+     * (3) tests with multiple cards in hand
+     */
+    @Test
+    void cardCountTest() {
+        assertEquals(0, player.getCardCount());
+        player.addCard(new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS));
+        assertEquals(1, player.getCardCount());
+        player.addCard(new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS));
+        player.addCard(new Card(CARDVALUE.FIVE, CARDTYPE.DEFAULT, CARDSUITE.CLUBS));
+        assertEquals(3, player.getCardCount());
+    }
+}
