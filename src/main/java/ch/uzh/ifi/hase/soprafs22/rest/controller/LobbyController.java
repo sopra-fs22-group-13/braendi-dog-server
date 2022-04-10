17193f1e49f5a653fd5fd1cb5cf8469bd744aa98
Anton Crazzolara <anton.crazzolara@outlook.com>
2022-04-10 18:13:21 +0200

    Created a LobbyGetDTO class. Doesn't do much. Added a Mapping for lobby creation deosn't work yet, but doesn't cause bugs either. Hooray!

--- src/main/java/ch/uzh/ifi/hase/soprafs22/rest/controller/LobbyController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/controller/LobbyController.java
@@ -1,4 +1,23 @@
 package ch.uzh.ifi.hase.soprafs22.rest.controller;
 
+import ch.uzh.ifi.hase.soprafs22.lobby.LobbyManager;
+import ch.uzh.ifi.hase.soprafs22.rest.data.dto.LobbyGetDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class LobbyController {
+
+    //private final LobbyManager lobbyManager = new LobbyManager();
+
+    @PostMapping("/menu/lobby")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public LobbyGetDTO createLobby(@RequestBody String usertoken) {
+
+        //int lobbyID = lobbyManager.openLobby(usertoken);
+
+        return new LobbyGetDTO(17);
+    }
+
 }
