39b583da4bc437c0ba68c35644613ffff2fde3d0
SandroVonlanthen <91312514+SandroVonlanthen@users.noreply.github.com>
2022-04-26 18:23:21 +0200

    Closing Game Instance with test

--- src/main/java/ch/uzh/ifi/hase/soprafs22/game/gameInstance/Game.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/game/gameInstance/Game.java
@@ -123,8 +123,17 @@ public class Game {
         catch (NullPointerException e){
             throw new InvalidMoveException("Move Not allowed", "Bad token");}
 
-        if (!checkValidTurns(move, playerWantToMove)||move.get_color()!=playerWantToMove.getColor()) {
-            throw new InvalidMoveException("Move Not allowed", "Bad move logic");
+        if (!checkValidTurns(move, playerWantToMove)) {
+            throw new InvalidMoveException("Move Not allowed", "Wrong move formation");
+        }
+
+        if (move.get_color()!=playerWantToMove.getColor()) {
+            throw new InvalidMoveException("Move Not allowed", "You want to move the wrong marble");
+        }
+
+
+        if (!playerWantToMove.isCardAvailable(move.get_card())){
+            throw new InvalidMoveException("Move Not allowed", "Card is not in hand");
         }
 
         // check if someone has a valid turn
@@ -162,8 +171,10 @@ public class Game {
             if (_board.checkWinningCondition(player.getColor())) {
 
                 _userManager.sendUpdateToAll(new UpdateDTO(UpdateType.WIN, String.format("{\"win\": \"%s\"}", player.getColor())));
+
+                _manager.deleteGame(_gameToken);
                 return;
-                //todo decusctruction Sandro
+
 
             }
         }
