53354a30f4a9545d92cf65bab52c7f0a06179ef1
Anton Crazzolara <anton.crazzolara@outlook.com>
2022-04-29 11:08:33 +0200

    Updated LobbyTest class

--- src/main/java/ch/uzh/ifi/hase/soprafs22/lobby/Lobby.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/lobby/Lobby.java
@@ -71,10 +71,6 @@ public class Lobby {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public User getOwner() {
         return this.owner;
     }
--- src/test/java/ch/uzh/ifi/hase/soprafs22/lobby/LobbyTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/lobby/LobbyTest.java
@@ -3,8 +3,11 @@ package ch.uzh.ifi.hase.soprafs22.lobby;
 import ch.uzh.ifi.hase.soprafs22.rest.entity.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,12 +22,13 @@ class LobbyTest {
 
     @Test
     public void createLobbyTest() {
-        assertEquals(Arrays.asList(owner), lobby.getPlayers());
+        assertEquals(List.of(owner), lobby.getPlayers());
         int startId = lobby.getId();
 
         User newOwner = new User();
         Lobby newLobby = new Lobby(newOwner);
         assertEquals(startId + 1, newLobby.getId());
+        assertEquals(owner, lobby.getOwner());
     }
 
     @Test
@@ -35,5 +39,26 @@ class LobbyTest {
 
         User anotherPlayer = new User();
         assertEquals(3, lobby.addPlayer(anotherPlayer));
+
+        User andAnotherOne = new User();
+        lobby.addPlayer(andAnotherOne);
+
+        User lastOne = new User();
+        assertThrows(ResponseStatusException.class, () -> lobby.addPlayer(lastOne));
+    }
+
+    @Test
+    public void invitesTest() {
+        //creates 2 new users and adds them as invitees
+        User newPlayer = new User();
+        User anotherPlayer = new User();
+        lobby.addInvitee(newPlayer);
+        lobby.addInvitee(anotherPlayer);
+
+        assertEquals(List.of(newPlayer, anotherPlayer), lobby.getPendingInvites());
+
+        //removes one invitee
+        lobby.deleteInvite(newPlayer);
+        assertEquals(List.of(anotherPlayer), lobby.getPendingInvites());
     }
 }
