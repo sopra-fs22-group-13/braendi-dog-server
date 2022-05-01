9da079a4d42a569aa85fe372dc54252de70ef872
Luca Zwahlen <luca.zwahlen@uzh.ch>
2022-05-01 11:31:01 +0200

    fixed a bug where cards would be removed where the shouldnt commented out the test agian. Thx GitHub actions

--- src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
@@ -15,26 +15,6 @@ public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
-
-    //testing vc here. comment this try block out if it does not work.
-      /*try {
-          Thread.sleep(10000);
-          VoiceRoom vr =  new VoiceChatCreator().createRoomWithPlayers("hello");
-          IUpdateController updCtrl = SpringContext.getBean(UpdateController.class);
-          UpdateDTO updto = new UpdateDTO(UpdateType.START, String.format("{\"app_id\": \"%s\", \"user_id\": \"%s\", \"user_auth\": \"%s\", \"room_id\": \"%s\"}", vr.appId, vr.player1.id, vr.player1.accessToken, vr.roomId));
-          UpdateDTO updto1 = new UpdateDTO(UpdateType.START, String.format("{\"app_id\": \"%s\", \"user_id\": \"%s\", \"user_auth\": \"%s\", \"room_id\": \"%s\"}", vr.appId, vr.player2.id, vr.player2.accessToken, vr.roomId));
-
-          Thread.sleep(2000);
-
-          updCtrl.sendUpdateToUser("12345", updto);
-          updCtrl.sendUpdateToUser("123", updto1);
-
-          Thread.sleep(30000);
-          new VoiceChatCreator().destroyRoomWithPlayers("hello");
-      }
-      catch (InterruptedException e) {
-          e.printStackTrace();
-      }*/
   }
 
   @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
