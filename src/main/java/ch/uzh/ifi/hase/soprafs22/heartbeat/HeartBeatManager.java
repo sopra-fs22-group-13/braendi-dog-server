c60863d7e1ee90bf0077c95e11affd701fe94f00
Shitao Zeng <shitao.zeng@uzh.ch>
2022-05-16 20:31:46 +0200

    added some game closed

--- src/main/java/ch/uzh/ifi/hase/soprafs22/heartbeat/HeartBeatManager.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/heartbeat/HeartBeatManager.java
@@ -10,48 +10,42 @@ public class HeartBeatManager {
 
     private Map<String, PlayerHeartBeat> playerHeartBeatMap = new HashMap<>();
 
-    private HeartBeatManager(){}
+    private HeartBeatManager() {
+    }
 
-    public static HeartBeatManager getInstance()
-    {
-        if(instance == null)
-        {
+    public static HeartBeatManager getInstance() {
+        if (instance == null) {
             instance = new HeartBeatManager();
         }
 
         return instance;
     }
 
-    public void addHeartBeat(String playerToken, HeartBeatType type)
-    {
-        PlayerHeartBeat phb =  playerHeartBeatMap.get(playerToken);
-        if(phb == null)
-        {
-            //create a new one
+    public void addHeartBeat(String playerToken, HeartBeatType type) {
+        PlayerHeartBeat phb = playerHeartBeatMap.get(playerToken);
+        if (phb == null) {
+            // create a new one
             phb = new PlayerHeartBeat(4000L);
             playerHeartBeatMap.put(playerToken, phb);
             updateOnlineStatus(playerToken, true);
         }
 
-        //add heartbeat
+        // add heartbeat
         phb.updateHeartBeat(type);
     }
 
-    public String[] getAllActivePlayerTokens()
-    {
+    public String[] getAllActivePlayerTokens() {
         return playerHeartBeatMap.keySet().toArray(new String[0]);
     }
 
-    
-    public Map<HeartBeatType, Boolean> getHeartBeatsOfPlayer(String playerToken)
-    {
+    public Map<HeartBeatType, Boolean> getHeartBeatsOfPlayer(String playerToken) {
         PlayerHeartBeat phb = playerHeartBeatMap.get(playerToken);
-        if(phb == null) return null;
+        if (phb == null)
+            return null;
 
         Map<HeartBeatType, Boolean> res = phb.isValidHeartBeat();
-        if(isAllFalse(res))
-        {
-            //player went offline
+        if (isAllFalse(res)) {
+            // player went offline
             updateOnlineStatus(playerToken, false);
             playerHeartBeatMap.remove(playerToken);
         }
@@ -59,18 +53,17 @@ public class HeartBeatManager {
         return res;
     }
 
-    public void updateOnlineStatus(String token, Boolean online)
-    {
-        
+    public void updateOnlineStatus(String token, Boolean online) {
+
     }
 
-    private boolean isAllFalse(Map<HeartBeatType, Boolean> map)
-    {
+    private boolean isAllFalse(Map<HeartBeatType, Boolean> map) {
         boolean allFalse = true;
         for (Boolean b : map.values()) {
-            if(b) allFalse = false;
+            if (b)
+                allFalse = false;
         }
 
-        return  allFalse;
+        return allFalse;
     }
 }
