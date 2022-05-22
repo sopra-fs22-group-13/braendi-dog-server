07fe7f0fa15ba97b76835ba22bcf7017a48eea4f
Anton Crazzolara <anton.crazzolara@outlook.com>
2022-05-22 21:30:14 +0200

    Wrote tests for UserController

--- src/test/java/ch/uzh/ifi/hase/soprafs22/rest/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/rest/controller/UserControllerTest.java
@@ -1,6 +1,8 @@
 package ch.uzh.ifi.hase.soprafs22.rest.controller;
 
 import ch.uzh.ifi.hase.soprafs22.rest.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs22.rest.data.dto.UserPutDTO;
+import ch.uzh.ifi.hase.soprafs22.rest.entity.GameHistory;
 import ch.uzh.ifi.hase.soprafs22.rest.entity.User;
 import ch.uzh.ifi.hase.soprafs22.rest.data.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs22.rest.service.GameHistoryService;
@@ -18,15 +20,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
@@ -48,6 +48,23 @@ public class UserControllerTest {
   @MockBean
   private GameHistoryService gameHistoryService;
 
+
+    /**
+     * the following tests verify the behaviour of
+     * GET /users
+     */
+
+  //failed authorization
+  @Test
+  public void getUsers_Test_Unauthorized() throws Exception {
+      // when
+      MockHttpServletRequestBuilder getRequest = get("/users");
+
+      // then
+      basicAuthorization_Test(getRequest);
+  }
+
+  //happy weather
   @Test
   public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
     String token=UUID.randomUUID().toString();
@@ -68,12 +85,55 @@ public class UserControllerTest {
     MockHttpServletRequestBuilder getRequest = get("/users")
                     .header("Authorization", token)
                     .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+
     // then
     mockMvc.perform(getRequest).andExpect(status().isOk())
         .andExpect(jsonPath("$", hasSize(1)))
         .andExpect(jsonPath("$[0].username", is(user.getUsername())))
         .andExpect(jsonPath("$[0].status", is(user.getStatus().toString())));
   }
+
+
+    /**
+     * the following tests verify the behaviour of
+     * GET /users/{userID}
+     */
+
+  //failed authorization
+  @Test
+  public void getUserByUserID_Test_Unauthorized() throws Exception {
+      //when
+      MockHttpServletRequestBuilder getRequest = get("/users/1");
+
+      //then
+      basicAuthorization_Test(getRequest);
+  }
+
+  //happy weather
+  @Test
+  public void getUserByUserID_Test() throws Exception {
+      //given
+      String token = UUID.randomUUID().toString();
+      User user = new User();
+      user.setId(1L);
+
+      given(userService.getUserById(1L)).willReturn(user);
+      given(userService.checkIfLoggedIn(Mockito.any())).willReturn(user);
+
+      //when
+      MockHttpServletRequestBuilder getRequest = get("/users/1")
+              .header("Authorization", token)
+              .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+
+      //then
+      mockMvc.perform(getRequest).andExpect(status().isOk())
+              .andExpect(jsonPath("$.id", is(user.getId().intValue())));
+  }
+
+    /**
+     * the following tests verify the behaviour of
+     * POST /users
+     */
 
   @Test
   public void createUser_validInput_userCreated() throws Exception {
@@ -106,6 +166,157 @@ public class UserControllerTest {
         .andExpect(jsonPath("$.status", is(user.getStatus().toString())));
   }
 
+
+    /**
+     * the following tests verify the behaviour of
+     * PUT /users/{userID}
+     */
+
+  //failed basic authorization
+  @Test
+  public void putUserByUserID_Test_Unauthorized() throws Exception {
+      UserPutDTO userPutDTO = new UserPutDTO();
+
+      //when
+      MockHttpServletRequestBuilder putRequest = put("/users/1")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(asJsonString(userPutDTO));
+
+      //then
+      basicAuthorization_Test(putRequest);
+  }
+
+  // put request trying to edit someone else's profile
+  @Test
+  public void putUserByUserID_Test_AccessDenied() throws Exception {
+      // given
+      User user = new User();
+      user.setId(2L);
+
+      UserPutDTO userPutDTO = new UserPutDTO();
+
+      given(userService.checkIfLoggedIn(Mockito.any())).willReturn(user);
+
+      //when
+      MockHttpServletRequestBuilder putRequest = put("/users/1")
+              .header("Authorization", "testToken")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(asJsonString(userPutDTO));
+
+      //then
+      mockMvc.perform(putRequest).andExpect(status().isUnauthorized());
+  }
+
+  //valid put request
+  @Test
+  public void putUserByUserID_Test_Valid() throws Exception {
+      // given
+      User user = new User();
+      user.setId(1L);
+
+      UserPutDTO userPutDTO = new UserPutDTO();
+
+      given(userService.checkIfLoggedIn(Mockito.any())).willReturn(user);
+
+      //when
+      MockHttpServletRequestBuilder putRequest = put("/users/1")
+              .header("Authorization", "testToken")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(asJsonString(userPutDTO));
+
+      //then
+      mockMvc.perform(putRequest).andExpect(status().isOk());
+
+      Mockito.verify(userService, times(1)).updateUser(Mockito.any());
+  }
+
+
+    /**
+     * the following tests verify the behaviour of
+     * GET /users/{userID}/history
+     */
+
+    //failed basic authorization
+    @Test
+    public void getMatchHistory_Test_Unauthorized() throws Exception {
+        //when
+        MockHttpServletRequestBuilder getRequest = get("/users/1/history");
+
+        //then
+        basicAuthorization_Test(getRequest);
+    }
+
+    @Test
+    public void getMatchHistory_Test() throws Exception {
+        // given
+        User user = new User();
+        given(userService.checkIfLoggedIn(Mockito.any())).willReturn(user);
+
+        List<GameHistory> providedHistory = new ArrayList<>();
+        for (int i=0; i<20; i++) providedHistory.add(randomGameHistory());
+
+        given(gameHistoryService.getPlayedGames(user)).willReturn(providedHistory);
+
+        //when
+        MockHttpServletRequestBuilder getRequest = get("/users/1/history")
+                .header("Authorization", "testToken");
+
+        //then
+        mockMvc.perform(getRequest).andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(10)));
+    }
+
+
+    /**
+     * the following tests verify the behaviour of
+     * POST /login
+     */
+
+    @Test
+    public void login_Test() throws Exception {
+        // given
+        User user = new User();
+        user.setId(1L);
+        user.setPassword("Test User");
+        user.setUsername("testUsername");
+        user.setToken("1");
+        user.setStatus(UserStatus.ONLINE);
+
+        UserPostDTO userPostDTO = new UserPostDTO();
+        userPostDTO.setPassword("Test User");
+        userPostDTO.setUsername("testUsername");
+
+        given(userService.loginUser(Mockito.any())).willReturn(user);
+
+        // when
+        MockHttpServletRequestBuilder postRequest = post("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(userPostDTO));
+
+        // then
+        mockMvc.perform(postRequest)
+                .andExpect(status().isOk())
+                .andExpect(header().exists("Authorization"))
+                .andExpect(header().string("Authorization", "Basic 1"))
+                .andExpect(jsonPath("$.id", is(user.getId().intValue())))
+                .andExpect(jsonPath("$.username", is(user.getUsername())))
+                .andExpect(jsonPath("$.status", is(user.getStatus().toString())));
+    }
+
+
+    /**
+     * tests if the method fails, given a failed basic authorization
+     */
+    private void basicAuthorization_Test(MockHttpServletRequestBuilder mockBuilder) throws Exception {
+        given(userService.checkIfLoggedIn(Mockito.any())).willThrow( new ResponseStatusException(HttpStatus.UNAUTHORIZED));
+
+        mockMvc.perform(mockBuilder)
+                .andExpect(status().isUnauthorized());
+    }
+
+
+
+
   /**
    * Helper Method to convert userPostDTO into a JSON string such that the input
    * can be processed
@@ -121,5 +332,12 @@ public class UserControllerTest {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
           String.format("The request body could not be created.%s", e.toString()));
     }
+  }
+
+  private GameHistory randomGameHistory() {
+      GameHistory gh = new GameHistory();
+      gh.setStartDate(new Random().nextLong());
+
+      return gh;
   }
 }
